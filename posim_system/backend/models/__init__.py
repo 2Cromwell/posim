a24f2095de5f9c49b7a34f0f12da9b16d14ca762
2Cromwell <2238892790@qq.com>
2026-03-06 10:23:12 +0800

    Completely reset git index to respect .gitignore
    This removes all tracked files that should be ignored by .gitignore,
    including .claude, .vscode, .windsurf, data_process, posim_system,
    and other unwanted output files.

--- posim_system/backend/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .project import Project
-from .simulation import Simulation
-from .dataset import Dataset
-from .intervention import Intervention
-
-__all__ = ["Project", "Simulation", "Dataset", "Intervention"]
--- posim_system/backend/schemas/__init__.py
+++
@@ -1,5 +0,0 @@
-from .project import *
-from .simulation import *
-from .dataset import *
-from .intervention import *
-from .analysis import *
